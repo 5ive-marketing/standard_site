5f9b39355ddb44450328bf4add899ade106676ff
Dominic Groshong <groshond@gmail.com>
2020-01-10 13:27:18 -0800

    Alpha final style tweaks

--- five/settings.py
+++ five/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = [
     'localhost:8000',
     'localhost',
