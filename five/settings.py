9321a4430ad1bb4408051343901aaffaf311e6ba
Dominic Groshong <groshond@gmail.com>
2020-01-10 12:34:40 -0800

    style fix

--- five/settings.py
+++ five/settings.py
@@ -17,7 +17,6 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
